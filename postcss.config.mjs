9fdf2ca75d7d1a4c5c588e9b63330e55bf1d47c3
PrajjwalDeep.Singh <prajjwaldeep.singh@gfk.com>
2025-04-12 01:17:06 +0530

    header-animation-styles-added

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,23 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    "@tailwindcss/postcss": {
+      theme: {
+        extend: {
+          keyframes: {
+            gradient: {
+              "0%": { backgroundPosition: "0% 50%" },
+              "50%": { backgroundPosition: "100% 50%" },
+              "100%": { backgroundPosition: "0% 50%" },
+            },
+          },
+          animation: {
+            gradient: "gradient 8s linear infinite",
+          },
+        },
+      },
+      plugins: [],
+    },
+  },
 };
 
 export default config;
