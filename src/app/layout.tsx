51a8a26dce3b49539c1e1143001f9cc8b816cd87
Prajjwal Deep Singh <prajjwal1997@gmail.com>
2025-04-12 20:25:29 +0530

    dark-mode-added (#2)
    Co-authored-by: PrajjwalDeep.Singh <prajjwaldeep.singh@gfk.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navigation from "./components/navigation";
+import Navigation from "../components/common/navigation";
 
 export const metadata: Metadata = {
   title: "Create Next App",
