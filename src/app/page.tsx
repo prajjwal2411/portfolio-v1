6632c893906e1bb907421419751e871e2750b148
PrajjwalDeep.Singh <prajjwaldeep.singh@gfk.com>
2025-04-15 01:19:04 +0530

    added-resume-button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,33 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import GithubIcon from "./components/icons/linkedin";
+import LinkedInIcon from "./components/icons/github";
 
 export default function Home() {
   return (
     <>
       <div className="grid grid-cols-2 grid-rows-2 gap-2 h-screen">
         <div className="row-span-2 flex flex-col justify-center ">
-          <Card className="flex flex-col">
-            <CardHeader>
-              {/* My Photo */}
-              <div></div>
-            </CardHeader>
-            <CardContent>
-              <div className="text-center">
-                <h1>Prajjwal Singh</h1>
-                <h4>Frontend Developer</h4>
-                <h4>Pune, India</h4>
-              </div>
-              <div>
-                {/* Add Linkedin, Github, Profiles */}
-              </div>
-            </CardContent>
+          <Card className="flex flex-col items-center px-6">
+            {/* My Photo */}
+            <div></div>
+            <div className="text-center">
+              <h1>Prajjwal Singh</h1>
+              <h4>Frontend Developer</h4>
+              <h4>Pune, India</h4>
+            </div>
+            <div className="flex flex-row">
+              <span className="m-2">
+                <GithubIcon />
+              </span>
+              <span className="m-2">
+                <LinkedInIcon />
+              </span>
+            </div>
+            <div>
+              {/* Add a button to open the resume's PDF */}
+              <Button>Resume</Button>
+            </div>
           </Card>
         </div>
         <div className=""></div>
