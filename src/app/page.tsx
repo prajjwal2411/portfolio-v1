d15f923fa577461918f534f917f77c78d9d4bbbd
PrajjwalDeep.Singh <prajjwaldeep.singh@gfk.com>
2025-04-18 22:22:03 +0530

    count-up-component-added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,13 @@ import {
 } from "@/components/ui/Card/card";
 import GithubIcon from "./components/icons/linkedin";
 import LinkedInIcon from "./components/icons/github";
+import CountUp from "./components/animations/CountUp/CountUp";
 
 export default function Home() {
   return (
     <>
       <div className="grid grid-cols-2 grid-rows-2 gap-2 h-screen">
-        <div className="row-span-2 flex flex-col justify-center w-1/2">
+        <div className="row-span-2 flex flex-col justify-center w-1/2 m-auto">
           <Card className="flex flex-col items-center px-6">
             {/* My Photo */}
             <div></div>
@@ -20,6 +21,7 @@ export default function Home() {
               <h1>Prajjwal Singh</h1>
               <h4>Frontend Developer</h4>
               <h4>Pune, India</h4>
+              <h4>prajjwals2411@gmail.com</h4>
             </div>
             <div className="flex flex-row">
               <span className="m-2">
@@ -42,18 +44,48 @@ export default function Home() {
             </CardHeader>
             <CardContent>
               <p>
-              I&apos;m a frontend web developer dedicated to turning ideas into
-              creative solutions. I specialize in creating seamless and
-              intuitive user experiences. My approach focuses on creating
-              scalable, high-performing solutions tailored to both user needs
-              and business objectives. By prioritizing performance,
-              accessibility, and responsiveness, I strive to deliver experiences
-              that not only engage users but also drive tangible results.
+                I&apos;m a frontend web developer dedicated to turning ideas
+                into creative solutions. I specialize in creating seamless and
+                intuitive user experiences. My approach focuses on creating
+                scalable, high-performing solutions tailored to both user needs
+                and business objectives. By prioritizing performance,
+                accessibility, and responsiveness, I strive to deliver
+                experiences that not only engage users but also drive tangible
+                results.
               </p>
             </CardContent>
           </Card>
         </div>
-        <div className="col-start-2"></div>
+        <div className="col-start-2">
+          <div>
+            {/* Count up for years of experience */}
+            <div>
+              <CountUp
+                from={0}
+                to={4}
+                separator=","
+                direction="up"
+                duration={.1}
+                className="count-up-text"
+              />
+            </div>
+
+            {/* Count up for number of skills*/}
+            <div>
+            <CountUp
+                from={0}
+                to={20}
+                separator=","
+                direction="up"
+                duration={.1}
+                className="count-up-text"
+              />
+            </div>
+          </div>
+
+          {/* Marquee for companies I've worked for */}
+          <div></div>
+        </div>
       </div>
     </>
   );
