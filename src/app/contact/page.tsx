c3d61614328e07288ac2f94a11d9a58be97d5654
PrajjwalDeep.Singh <prajjwaldeep.singh@gfk.com>
2025-04-24 00:11:59 +0530

    intro-card-component-added

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,5 @@
 export default function Contact(){
     return <>
-    <h1>Let&apos;s Get in touch</h1>
+        <div></div>
     </>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,13 @@
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/Card/card";
-import GithubIcon from "./components/icons/linkedin";
-import LinkedInIcon from "./components/icons/github";
-import CountUp from "./components/animations/CountUp/CountUp";
+// import CountUp from "./components/animations/CountUp/CountUp";
+
+import IntroCard from "./components/common/IntroCard/IntroCard";
 
 export default function Home() {
   return (
     <>
       <div className="grid grid-cols-2 grid-rows-2 gap-2 h-screen">
         <div className="row-span-2 flex flex-col justify-center w-1/2 m-auto">
-          <Card className="flex flex-col items-center px-6">
-            {/* My Photo */}
-            <div></div>
-            <div className="text-center">
-              <p className="text-5xl mb-12">Prajjwal Singh</p>
-              <p className="text-xl font-light">Frontend Developer</p>
-              <p className="text-xl font-light">Pune, India</p>
-              <p className="text-xl font-light">prajjwals2411@gmail.com</p>
-            </div>
-            <div className="flex flex-row mb-12">
-              <span className="m-2">
-                <GithubIcon />
-              </span>
-              <span className="m-2">
-                <LinkedInIcon />
-              </span>
-            </div>
-            <div>
-              {/* Add a button to open the resume's PDF */}
-              <Button disabled>Resume</Button>
-            </div>
-          </Card>
+          <IntroCard />
         </div>
         <div className="flex flex-col justify-center">
           <div className="">
