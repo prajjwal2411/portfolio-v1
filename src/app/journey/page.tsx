35eed8b8b035c6bdc502f3a949bc723918790b08
PrajjwalDeep.Singh <prajjwaldeep.singh@gfk.com>
2025-11-15 22:09:37 +0530

    Completed design for Journey page

--- src/app/journey/page.tsx
+++ src/app/journey/page.tsx
@@ -1,58 +1,44 @@
-import styles from './journey.module.css';
+import { Badge } from "@/components/ui/badge";
+import JourneyCard from "../components/common/JourneyCards/JourneyCard";
+import styles from "./journey.module.css";
+import { journeyData } from "./journeyData";
+import Aurora from "../components/animations/Aurora/Aurora";
 
 export default function Journey() {
   return (
     <>
       <div className="">
-        <div className={`${styles.timeline}`} >
-          
-          <div className={`${styles.container} ${styles.rightContainer}`}>
-            <div className={`${styles.textBox}`}>
-              <h2>Bachelors Of Computer Applications</h2>
-              <small>2015-2018</small>
-              <p>Lorem ipsum</p>
-            </div>
-          </div>
-
-          <div className={`${styles.container} ${styles.leftContainer}`}>
-            <div className={`${styles.textBox}`}>
-              <h2>Masters Of Computer Applications</h2>
-              <small>2018-2021</small>
-              <p>Lorem ipsum</p>
-            </div>
-          </div>
-
-          <div className={`${styles.container} ${styles.rightContainer}`}>
-            <div className={`${styles.textBox}`}>
-              <h2>Value Coders Pvt. Ltd.</h2>
-              <small>2021-2024</small>
-              <p>Lorem ipsum</p>
-            </div>
-          </div>
-
-          <div className={`${styles.container} ${styles.leftContainer}`}>
-            <div className={`${styles.textBox}`}>
-              <h2>Nielsen IQ</h2>
-              <small>2024-Present</small>
-              <p>Lorem ipsum</p>
-            </div>
-          </div>
-
-          <div className={`${styles.container} ${styles.rightContainer}`}>
-            <div className={`${styles.textBox}`}>
-              <h2>Value Coders Pvt. Ltd.</h2>
-              <small>2021-2024</small>
-              <p>Lorem ipsum</p>
-            </div>
-          </div>
-
-          <div className={`${styles.container} ${styles.leftContainer}`}>
-            <div className={`${styles.textBox}`}>
-              <h2>Nielsen IQ</h2>
-              <small>2024-Present</small>
-              <p>Lorem ipsum</p>
-            </div>
-          </div>
+        <div className={`${styles.timeline}`}>
+          {journeyData?.map((data, index) => {
+            return (
+              <div key={index + 1}>
+                <Badge
+                  className={`${styles.dateContainer} ${
+                    index % 2
+                      ? styles.dateLeftContainer
+                      : styles.dateRightContainer
+                  }`}
+                >
+                  {data?.date}
+                </Badge>
+                <div
+                  className={`${styles.container} ${
+                    index % 2 ? styles.rightContainer : styles.leftContainer
+                  }`}
+                >
+                  <div
+                    className={`${styles.timelinePointer} ${
+                      index % 2 ? styles.leftContainer : ""
+                    }`}
+                  ></div>
+                  <JourneyCard
+                    header={data?.header}
+                    title={data?.title}
+                  ></JourneyCard>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
