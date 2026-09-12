8eba2db57ce3854ec21e67bbe8ccb9b1c22b073a
Prajjwal Singh <prajjwal1997@gmail.com>
2026-09-12 15:59:42 +0530

    Complete Contact Page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,19 +1,64 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/Card/card";
-import { Field, FieldDescription, FieldGroup, FieldLabel, FieldLegend, FieldSet } from "@/components/ui/field";
+import { Field, FieldGroup, FieldLabel, FieldLegend, FieldSet } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import TwitterIcon from "../../app/components/icons/x";
+import GithubIcon from "../components/icons/github";
+import LinkedInIcon from "../components/icons/linkedin";
+import CodepenIcon from "../components/icons/codepen";
 
 export default function Contact() {
     return <>
         <div className="px-6 py-8">
-            {/* <div className="flex justify-center items-center m-auto"><h3>Have a role, partnership, or collaboration in mind? I&apos;d love to hear from you.</h3></div> */}
-            <div className="flex flex-column justify-between mt-12">
-                <div className="w-full">
-                    <h4>Reach out to me</h4>
-                    
+            <div className="flex flex-column justify-between mt-8">
+                <div className="w-full mt-2">
+                    <div className="my-1">
+                        <h4>Let's connect and create something awesome</h4>
+                        <p className="mt-4">Whether ypu're brand looking for a developer advocate, 
+                        a company in need of FullStack Developer, or just want to chat about tech, 
+                        I'd love to hear from you.</p>
+                        <h4 className="underline underline-offset-8 mt-5">prajjwals2411@gmail.com</h4>
+                    </div>
+                    <div className="grid grid-cols-2 gap-5 mt-8 w-fit">
+                        <Card className="flex flex-row w-fit p-5 items-center">
+                            <div><TwitterIcon /></div>
+                            <div className="flex flex-col">
+                                <div>X (Twitter)</div>
+                                <div>@username</div>
+                            </div>                            
+                        </Card>
+                        <Card className="flex flex-row w-fit p-5 items-center">
+                            <div><CodepenIcon /></div>
+                            <div className="flex flex-col">
+                                <div>Codepen</div>
+                                <div>@username</div>
+                            </div>                            
+                        </Card>
+                        <Card className="flex flex-row w-fit p-5 items-center">
+                            <div><GithubIcon /></div>
+                            <div className="flex flex-col">
+                                <div>Github</div>
+                                <div>@username</div>
+                            </div>                            
+                        </Card>
+                        <Card className="flex flex-row w-fit p-5 items-center">
+                            <div><LinkedInIcon /></div>
+                            <div className="flex flex-col">
+                                <div>Linkedin</div>
+                                <div>@username</div>
+                            </div>                            
+                        </Card>
+                        <Card className="flex flex-row w-fit p-5 items-center">
+                            <div><TwitterIcon /></div>
+                            <div className="flex flex-col">
+                                <div>Leetcode</div>
+                                <div>@username</div>
+                            </div>                            
+                        </Card>
+                    </div>
                 </div>
-                <div className="m-auto w-1/2">
+                <div className="m-auto w-1/2 h-full">
                     <Card>
                         <div className="pb-12 px-4">
                             <form action="">
